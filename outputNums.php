9e25d7cd634fb0f7d308ae5c60a207ff94314891
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-07-24 17:06:38 +0300

    Add files via upload

---
+++ outputNums.php
@@ -0,0 +1,15 @@
+<?php
+$number1 = 10;
+$number2 = 20;
+if ($number1 > $number2)
+{
+    echo "Більше число: $number1";
+}
+elseif ($number2 > $number1)
+{
+    echo "Більше число: $number2";
+}
+else
+{
+    echo "Числа рівні: $number1 і $number2";
+}
